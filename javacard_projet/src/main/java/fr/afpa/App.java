9d903549f7e04316423d70eece11a288caea7b63
Hadja41 <hadhawabah@gmail.com>
2024-08-01 09:09:36 +0200

    Sérialisation JSON

--- javacard_projet/src/main/java/fr/afpa/App.java
+++ javacard_projet/src/main/java/fr/afpa/App.java
@@ -6,7 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 /**
@@ -35,14 +37,39 @@ public class App extends Application {
     public static void main(String[] args) {
         launch();
 
-        //Sérialisation JSON
-        Contact contact = new Contact()
-        contact.setName("Toto");
-        contact.setSurname("Tata");
-        contact.setAddress("69 rue toto");
-        contact.setGender("Male");
-        contact., LocalDate.of(2000, 2, 4), "Tota", "123456789",
-                "987654321", "john.doe@example.com", "Bordeaux", "12345", "johnDoeGitHub");
+        //Sérialisation Objet Java vers JSON
+        Contact contact = new Contact("Titi", "Tutu", "56 rue titi", "Female", LocalDate.of(2002, 9, 24), "Titu",
+                "123456789", "987654321", "jane.doe@example.com", "Begles", "12345", "janeDoeGitHub");
+var mapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT).registerModule(new JavaTimeModule());
+
+
+    //     Contact contact = new Contact();
+    //     contact.setName("Toto");
+    //     contact.setSurname("Tata");
+    //     contact.setAddress("69 rue toto");
+    //     contact.setGender("Male");
+    //     contact.setBirthday(LocalDate.of(2000, 2, 4));
+    //     contact.setNickname("Tota");
+    //     contact.setPhoneNumber("123456789");
+    //     contact.setPhoneNumberProfessional("987654321");
+    //     contact.setEmail("john.doe@example.com");
+    //    // contact.setCity("Bordeaux");
+    //     contact.setPostalCode("12345");
+    //     contact.setGithub("johnDoeGitHub");
+
+     //Chemin du fichier pour stocker nos données JSON  (à revoir)
+    String filePath = "scr/test/resources/protocols/jackson_contact.json";
+        
+    //Stockage de l'objet contact dans un fchier JSON à l'aide de la classe ObjectMapper
+    File file = new File(filePath);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(file, contact);
+
+        //Déserialisation du contenu JSON en un objet java
+        Contact deserializedContact = mapper.readValue(new File(filePath), Contact.class);
+        assertEquals("Toto", deserializedContact.getName());
     }
+
+   
 
 }
